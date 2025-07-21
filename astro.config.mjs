2729b0e0c7f3520e46c88c8c953e793f77e7f6f4
jlvn <jlmrvdv@gmail.com>
2025-07-21 17:08:23 +0200

    feat: add english translation of the whole site

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,12 +11,30 @@ export default defineConfig({
   site: 'https://theoaktells.com',
   integrations: [
       sitemap({
-          lastmod: new Date(Date.now())
+          lastmod: new Date(Date.now()),
+          i18n: {
+            defaultLocale: 'nl',
+            locales: {
+              nl: 'nl-NL',
+              en: 'en-US'
+            },
+        },
       }),
       react()
   ],
   redirects: {
     '/over': '/about'
+  },
+  i18n: {
+    locales: ['nl', 'en'],
+    defaultLocale: 'nl',
+    routing: {
+      fallbackType: 'rewrite',
+      prefixDefaultLocale: false
+    },
+    fallback: {
+      en: 'nl'
+    }
   },
   vite: {
     plugins: [tailwindcss()]
